766d96b7ebd30672b6f4f42f1070d2b0ddef0cb6
xuanthuy14022007-hub <xuanthuy14022007@gmail.com>
2026-03-01 11:18:27 +0700

    Update logic_dang_nhap.py

--- logic_dang_nhap.py
+++ logic_dang_nhap.py
@@ -1,5 +1,5 @@
 from database_connector import get_connection
-
+import sqlite3
 #Hàm đăng nhập
 def login(username, password):
     conn = get_connection()  #mở kết nối database
@@ -50,5 +50,6 @@ def reset_password(username, new_password):
     conn.close()
 
     return success  #True nếu đổi được, False nếu không có username
+
 
 
