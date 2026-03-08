a84b158263a5fa17bc8d17d504ec61c46194fd50
xuanthuy14022007-hub <xuanthuy14022007@gmail.com>
2026-03-08 14:45:29 +0700

    Update database_init.py

--- database_init.py
+++ database_init.py
@@ -20,7 +20,8 @@ def init_db():
         password TEXT NOT NULL,
         role TEXT NOT NULL,        
         full_name TEXT,
-        email TEXT UNIQUE
+        email TEXT UNIQUE, 
+        address TEXT
     )''')
 
     # --- 2. BẢNG DANH MỤC CÂY TRỒNG ---
