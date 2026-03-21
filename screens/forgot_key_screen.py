114fa21eab1d32c2d961aeeca204d070e1b24ade
myntqk25406-beep <myntqk25406@st.uel.edu.vn>
2026-03-21 11:21:48 +0700

    Update forgot_key_screen.py

--- screens/forgot_key_screen.py
+++ screens/forgot_key_screen.py
@@ -2,7 +2,6 @@ from PyQt6.QtWidgets import QWidget
 from PyQt6 import uic
 
 from utils.window_manager import switch_window
-from logic.logic_dang_nhap import kiem_tra_ton_tai_user
 from screens.otp_screen import OtpScreen
 from screens.login_screen import LoginScreen
 from PyQt6.QtWidgets import QMessageBox
@@ -24,10 +23,5 @@ class ForgotKeyScreen(QWidget):
         identifier = self.txt_email_sdt.text().strip() if hasattr(self, 'txt_email_sdt') else ''
         if not identifier:
             QMessageBox.warning(self, "Lỗi", "Vui lòng nhập Email hoặc Số điện thoại!")
-            return
-            
-        username = kiem_tra_ton_tai_user(identifier)
-        if not username:
-            QMessageBox.warning(self, "Lỗi", "Tài khoản không tồn tại!")
             return
         switch_window(OtpScreen(username))
