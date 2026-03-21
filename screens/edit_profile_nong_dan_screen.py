876860241857470a9cb0ebcd1c8110501fd1c4b0
nnlinh-06 <linhnnk25406@st.uel.edu.vn>
2026-03-21 09:45:09 +0700

    Update edit_profile_nong_dan_screen.py

--- screens/edit_profile_nong_dan_screen.py
+++ screens/edit_profile_nong_dan_screen.py
@@ -1,10 +1,9 @@
 from PyQt6.QtWidgets import QWidget, QMessageBox
 from PyQt6 import uic
 
-from utils.window_manager import get_current_user
+from utils.window_manager import switch_window, get_current_user
 from logic.logic_nguoi_dung import lay_thong_tin_nguoi_dung
 
-from utils.main import switch_window, ProfileNongDanScreen
 
 
 class EditProfileNongDanScreen(QWidget):
