a4e9980876d2b7c0661a711f8ed303e7154116d6
aninatranpham <yentphk25406@st.uel.edu.vn>
2026-03-21 09:59:16 +0700

    Update profile_chu_vua_screen.py

--- screens/profile_chu_vua_screen.py
+++ screens/profile_chu_vua_screen.py
@@ -4,8 +4,7 @@ from PyQt6 import uic
 from utils.window_manager import get_current_user, set_current_user, switch_window
 from logic.logic_nguoi_dung import lay_thong_tin_nguoi_dung
 from screens.home_chu_vua_screen import ChuVuaDashboardScreen
-from screens.danh_sach_cay_trong_screen import DanhSachCayTrongScreen
-from screens.danh_sach_don_hang_chu_vua_screen import DanhSachDonHangChuVuaScreen
+from screens.search_list_mat_hang_screen import SearchListMatHangScreen
 from screens.login_screen import LoginScreen
 
 class ProfileChuVuaScreen(QWidget):
