b27c2a643e336dfcedbef4adfa56a440b0c46902
myntqk25406-beep <myntqk25406@st.uel.edu.vn>
2026-03-23 22:12:26 +0700

    Update splash_screen.py

--- screens/splash_screen.py
+++ screens/splash_screen.py
@@ -1,24 +1,18 @@
-from PyQt6.QtWidgets import QWidget, QGraphicsOpacityEffect
-from PyQt6.QtCore import QTimer, QPropertyAnimation, QEasingCurve
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import Qt, QTimer, QPropertyAnimation, QEasingCurve
 from PyQt6 import uic
-
 from utils.window_manager import switch_window
-from screens.login_screen import LoginScreen
 
 class SplashScreen(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("ui_files/splash_screen.ui", self)
-
-        # Hiệu ứng fade-out sau 3 giây
-        self.effect = QGraphicsOpacityEffect()
-        self.setGraphicsEffect(self.effect)
-        self.effect.setOpacity(1.0)
-
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setWindowOpacity(1.0)
         QTimer.singleShot(3000, self.bat_dau_fade_out)
 
     def bat_dau_fade_out(self):
-        self.animation = QPropertyAnimation(self.effect, b"opacity")
+        self.animation = QPropertyAnimation(self, b"windowOpacity")
         self.animation.setDuration(1000)
         self.animation.setStartValue(1.0)
         self.animation.setEndValue(0.0)
@@ -27,4 +21,5 @@ class SplashScreen(QWidget):
         self.animation.start()
 
     def chuyen_sang_login(self):
-        switch_window(LoginScreen())
+        from screens.login_screen import LoginScreen
+        switch_window(LoginScreen)
